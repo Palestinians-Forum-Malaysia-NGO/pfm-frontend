a0d6800ede7bcb45b090f7f86752fab6212fc3b7
adnanmadi241 <adnan.madi@codeandcanvas.io>
2026-05-28 14:26:58 +0800

    style : components
    1. added nav bar & footer.

---
+++ src/layouts/public/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import PublicNavbar from "components/navbar/PublicNavbar";
+import Footer from "components/footer/Footer";
+import routes from "routes.js";
+
+export default function PublicLayout() {
+  const getRoutes = () =>
+    routes
+      .filter((r) => r.layout === "/")
+      .map((route, key) => (
+        <Route path={route.path} element={route.component} key={key} />
+      ));
+
+  return (
+    <div className="flex min-h-screen flex-col bg-white">
+      <PublicNavbar />
+      <main className="flex-1">
+        <Routes>
+          {getRoutes()}
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
